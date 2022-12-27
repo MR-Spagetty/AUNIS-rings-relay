d137e610fe75d1a4d8e70ede093bd133d28e9ccb
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2022-12-27 20:28:21 +1300

    Made version file for ease of installer creation

---
+++ rrsVersions.lua
@@ -0,0 +1,34 @@
+local versions = {
+    relay = {
+        version = 1,
+        changelog = {
+            "Initial release",
+            "Contains basic relay system"
+        }
+    },
+    config = {
+        version = 1,
+        changelog = {
+            "Initial release"
+        }
+    },
+    route = {
+        version = 1,
+        changelog = {
+            "Initial release",
+            "Contains:",
+            " - BFS (Breadth-first search) algorithm for finding the shortest route"
+        }
+    },
+    moreTable = {
+        version = 1,
+        changelog = {
+            "Initial release",
+            "Contains:",
+            " - moreTable.contains",
+            " - moreTable.index",
+            " - moreTable.keys",
+            " - moreTable.values"
+        }
+    }
+}
